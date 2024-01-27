7db0412c81ea145155c3fe06523e3daa803a888a
Larissa Kim <kimLarissa04@gmail.com>
2024-01-27 16:27:26 -0600

    form created flightNum variable

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-console.log("Test Text")
+function getData(form) {
+    var formData = new FormData(form);
+  
+    for (var pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+  
+    console.log(Object.fromEntries(formData));
+    
+    a = Object.fromEntries(formData);
+    flyNum = Object.values(a);
+    //console.log(flyNum[0]);
+    return flyNum[0]
+  }
+  
+  document.getElementById("myForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    getData(e.target);
+  });
 

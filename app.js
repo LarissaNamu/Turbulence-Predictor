49eb9b0ef14496596f2c0938bb3f6d20597da0e7
Larissa Kim <kimLarissa04@gmail.com>
2024-01-28 10:34:21 -0600

    made form better

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
 
-function getData(form) {
-    var formData = new FormData(form);
+// function getData(form) {
+//     var formData = new FormData(form);
   
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ": " + pair[1]);
-    }
+//     for (var pair of formData.entries()) {
+//       console.log(pair[0] + ": " + pair[1]);
+//     }
   
-    console.log(Object.fromEntries(formData));
-    a = Object.fromEntries(formData);
-    flyn = Object.values(a);
-    return "https://www.flightaware.com/live/flight/AAL" + flyn[0];
+//     console.log(Object.fromEntries(formData));
+//     a = Object.fromEntries(formData);
+//     flyn = Object.values(a);
+//     return "https://www.flightaware.com/live/flight/AAL" + flyn[0];
+//   }
+
+
+// document.getElementById("myForm").addEventListener("submit", function (e) {
+//     e.preventDefault();
+//     getData(e.target);
+    
+    
+// });
+
+function redirect() {
+  var answergiven = document.getElementById("answergiven");
+  var realanswer = document.getElementById("first");
+  var nextpage1 = document.getElementById("nextpage1");
+
+  if(answergiven.value == realanswer.value){
+      window.location = nextpage1.value;
+  }
+  else{
+      alert('Please enter a valid flight number.');
   }
 
-
-document.getElementById("myForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    getData(e.target);
-    
-    
-});
+  return false; // prevent further bubbling of event
+}
 
 
 
